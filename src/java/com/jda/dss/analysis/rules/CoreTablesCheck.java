79acaaf59cb76f221182a992828256def8e88968
1033616 <teja.velagapudi@blueyonder.com>
2022-06-23 16:26:05 -0500

    SQL checks rule added

---
+++ src/java/com/jda/dss/analysis/rules/CoreTablesCheck.java
@@ -0,0 +1,40 @@
+package com.jda.dss.analysis.rules;
+
+import com.blueyonder.wm.csi.core.LocalSyntaxCompiler;
+import com.redprairie.moca.analysis.Issue;
+import com.redprairie.moca.analysis.IssueLevel;
+import com.redprairie.moca.analysis.RuleSet;
+import com.redprairie.moca.analysis.StaticAnalysisRule;
+import com.redprairie.moca.server.exec.CommandUnit;
+import com.redprairie.moca.server.repository.LocalSyntaxCommand;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class CoreTablesCheck implements RuleSet {
+    @StaticAnalysisRule(id = "Sql check for Core Tables")
+    public Issue execute(LocalSyntaxCommand command) {
+
+        final Pattern pattern = Pattern.compile("(insert into|update|delete from) (vc|usr|dss|us)[\\w]+");
+        final String name = command.getName();
+        final LocalSyntaxCompiler COMPILER = new LocalSyntaxCompiler();
+        final String message = "Please Follow Proper SQL checks:- ";
+        List<CommandUnit> units = COMPILER.compile(command);
+
+        for (CommandUnit unit : units) {
+            if (StringUtils.isNotBlank(unit.getSql())) {
+                Matcher matcher = pattern.matcher(unit.getSql().toLowerCase());
+                if (matcher.find()) {
+                    String description = "Please avoid modifying core tables using sql in the Command: ";
+                    return Issue.newIssue(message + description + name, IssueLevel.ERROR);
+                }
+            }
+        }
+        return Issue.noIssue();
+    }
+
+}
--- src/java/com/jda/dss/analysis/rules/ForUpdateCheckRule.java
+++ src/java/com/jda/dss/analysis/rules/ForUpdateCheckRule.java
@@ -3,12 +3,10 @@ package com.jda.dss.analysis.rules;
 import com.blueyonder.wm.csi.core.LocalSyntaxCompiler;
 import com.redprairie.moca.analysis.Issue;
 import com.redprairie.moca.analysis.IssueLevel;
+import com.redprairie.moca.analysis.RuleSet;
 import com.redprairie.moca.analysis.StaticAnalysisRule;
 import com.redprairie.moca.server.exec.CommandUnit;
-
 import com.redprairie.moca.server.repository.LocalSyntaxCommand;
-import com.redprairie.moca.analysis.RuleSet;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
@@ -16,27 +14,27 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 @Component
-public class SqlChecksRule implements RuleSet {
-    @StaticAnalysisRule(id = "Sql check for Rollback and Commit")
+public class ForUpdateCheckRule implements RuleSet {
+    @StaticAnalysisRule(id = "Sql check for For Update")
     public Issue execute(LocalSyntaxCommand command) {
-
-        final Pattern pattern = Pattern.compile("(commit|rollback)");
+        final Pattern pattern = Pattern.compile("(\\bfor update of\\b)");
         final String name = command.getName();
         final LocalSyntaxCompiler COMPILER = new LocalSyntaxCompiler();
+        final String message = "Please Follow Proper SQL checks:- ";
         List<CommandUnit> units = COMPILER.compile(command);
 
         for (CommandUnit unit : units) {
             if (StringUtils.isNotBlank(unit.getSql())) {
-                Matcher select_matcher = pattern.matcher(unit.getSql().toLowerCase());
-                if(select_matcher.find()){
-                    String description = "Please remove commit or rollback statements from SQL queries in the Command Name: ";
-                    return Issue.newIssue(message+ description+ name,IssueLevel.ERROR);
+                Matcher matcher = pattern.matcher(unit.getSql().toLowerCase());
+                if(matcher.find()){
+                    String description = "Please remove for update statement(s) from sql queries in the Command:  ";
+                    return Issue.newIssue(message+ description+ name, IssueLevel.ERROR);
                 }
             }
         }
         return Issue.noIssue();
     }
-    private static final String message = "Please Follow Proper SQL checks:- ";
+
+
 }
